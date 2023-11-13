9ae90cbddb81ce7e7f1a93e7ee721a1a985393d4
Kuma-1 <burhanpasha.work@gmail.com>
2023-11-12 23:48:55 -0800

    placeholder change

--- pages/components/layout.js
+++ pages/components/layout.js
@@ -41,12 +41,14 @@ const layout = ({ children }) => {
             transition={{ duration: 0.6, type: 'easeIn', damping: 8, stiffness: 30 }}
         >
           <Image 
+            placeholder="blur"
+            // blurDataURL="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1 1'%3E%3Crect width='1' height='1' fill='%23ddd'/%3E%3C/svg%3E"
             src="/burhan_website11.webp" 
             alt="My Image"
             width={800}
             height={600}
-            placeholder="blur"
-            blurDataURL="/me-blur.jpg" />
+            blurDataURL="/mev1.jpg"
+             />
 
 
 
