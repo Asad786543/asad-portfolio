012e0e4aeb95250e88904f2b54b6b035a9906723
Kuma-1 <burhanpasha.work@gmail.com>
2023-11-14 03:46:38 -0800

    meta again

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>CEO | Co-Founder | WebMetric's Digital Pioneer.</title>
         <meta name="description" content="CEO | Co-Founder | WebMetric's Digital Pioneer." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
