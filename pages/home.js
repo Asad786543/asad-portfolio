784756e16b8c4788986ab25bb8cd61a9e5d70afe
Kuma-1 <burhanpasha.work@gmail.com>
2025-01-07 18:05:48 +0500

    error build

--- pages/home.js
+++ pages/home.js
@@ -12,7 +12,7 @@ const home = () => {
 
                 </p>
                 <p className="desc">
-					  Now, as the founder of <Link href={"https://www.web-metric.co/" } target='_blank'>WebMetric</Link>, I lead a team delivering top-tier solutions and ensuring client satisfaction every step of the way. Check out the   <Link href={"/about" }>About</Link> page to learn more about me or visit the  <Link href={"/work" }>Work</Link> page to explore my projects.
+					  Now, as the Founder of <Link href={"https://www.web-metric.co/" } target='_blank'>WebMetric</Link>, I lead a team delivering top-tier solutions and ensuring client satisfaction every step of the way. Check out the   <Link href={"/about" }>About</Link> page to learn more about me or visit the  <Link href={"/work" }>Work</Link> page to explore my projects.
                 </p>
                 <div className="homepage-icon">
                     <img src="/home-icon.svg" alt="" />
